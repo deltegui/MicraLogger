1d96fb8563d240677fe2849ec389c89f556922ee
Diego Delgado <frikiman34@hotmail.com>
2018-01-29 17:33:32 +0100

    readme updated

--- README.md
+++ README.md
@@ -9,4 +9,27 @@ You can install this library using npm:
 
 ## Usage
 
-From everywhere in your code import and initialize the logger with a name
+Fist, reuquire the module and then call the exported function with the desired module-name:
+
+```javascript
+const ulogger = require('micralogger');
+const log = ulogger.logger('Demo');
+```
+
+Then, you can call to log functions:
+
+```javascript
+log.info('Message');
+log.warning('Message');
+log.error('Message');
+log.success('Message');
+```
+
+That code result in the following output:
+
+```
+29/0/2018 17:31:11 [INFO] (Demo) Message
+29/0/2018 17:31:11 [WARNING] (Demo) Message
+29/0/2018 17:31:11 [ERROR] (Demo) Message
+29/0/2018 17:31:11 [SUCCESS] (Demo) Message
+```
--- middlewares.js
+++ middlewares.js
@@ -1,24 +1,15 @@
 const logConstructor = require('./logger');
 const defaultLabel = 'LOG-MIDDLEWARE';
 
-function recoverMiddleware(logLabel = defaultLabel) {
-  const log = logConstructor(logLabel);
-  return (err, req, res, next) => {
-    log('error', err);
-    res.sendStatus(500);
-  }
-}
-
 function requestLogMiddleware(logLabel = defaultLabel) {
   const log = logConstructor(logLabel);
   return (req, res, next) => {
-    const strFormat = `Request: ${req.ip} => ${req.method} | ${req.baseUrl}`;
-    log('info', strFormat);
+    const strFormat = `Request: ${req.ip} => ${req.method} | ${req.originalUrl}`;
+    log.info(strFormat);
     next();
   }
 }
 
 module.exports = {
-  recoverMiddleware,
   requestLogMiddleware,
 };
