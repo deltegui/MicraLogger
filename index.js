46c6d1de26d639369e5967a8778cc616fa95f9c1
Diego Delgado <frikiman34@hotmail.com>
2018-03-29 21:42:40 +0200

    fixed error with uncuaght exception

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const loggerMiddlewares = require('./middlewares');
 
 function enableUncaughtExceptionLogger() {
   process.on('uncaughtException', (err) => {
-    const deathLog = loggerConstructor('DEATH MESSAGE');
-    deathLog.error(`A fatal error happened. The error is: ${err}`);
-    deathLog.error(`Stack Trace: ${err.stack}`);
-    deathLog.error('This is a fatal error. The app will exit soon...');
+    const deathLog = loggerConstructor('DEATH MESSAGE').error;
+    deathLog(`A fatal error happened. The error is: ${err}`, 'error');
+    deathLog(`Stack Trace: ${err.stack}`, 'error');
+    deathLog('This is a fatal error. The app will exit soon...', 'error');
     process.exit(1);
   });
 }
